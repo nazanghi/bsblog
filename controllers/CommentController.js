4062e41d785d237b53b7bcb3db5a2c3ca2c85b32
Nicola Zanghi <nico.zang7@gmail.com>
2020-11-23 00:47:05 -0500

    completes controllers in backend

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,3 +1,4 @@
+
 const {TravelLog, Comment} = require('../database/schema')
 
 const CreateComment = async (request, response) =>{
@@ -17,13 +18,24 @@ const CreateComment = async (request, response) =>{
 }
 const  RemoveComment= async (request, response) =>{
     try{
-
+        await Comment.deleteOne({ _id: request.params.comment_id})
+        const updatedPost = await TravelLog.findByIdAndUpdate(
+            request.params.post_id,
+            { $pull: {comments: { _id: request.params.comment_id }}},
+            { upsert: true, new: true}
+        )
+        response.send(updatedPost)
     }catch(error){throw error}
 }
 const  UpdateComment= async (request, response) =>{
     try{
-
-    }catch(error){throw error}
+        await Comment.findByIdAndUpdate(
+            request.params.comment_id,
+            {...request.body},
+            {upsert: true, new: true},
+            (error, d) => (error ? error : response.send(d))
+            )
+        }catch(error){throw error}
 }
 
 module.exports = {
